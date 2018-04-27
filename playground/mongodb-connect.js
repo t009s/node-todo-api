b66c31e9bd4a3cd0354859aaedacd4c5c9c3cd65
t009s <t009s@github.com>
2018-04-27 05:54:23 +0530

    Add Post todos and mongoose

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,10 +1,6 @@
 //const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
-var obj = new ObjectID();
-console.log(obj);
-
-
 MongoClient.connect('mongodb://localhost:27017', (err, client) => {
     if (err) {
         return console.log('Unable to connect to mongoDb server');
