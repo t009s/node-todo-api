0549611c5cbf96bc4e3bdbd285e9ef33e92b5fb8
t009s <t009s@github.com>
2018-05-01 17:13:46 +0530

    Added MongoDB_URI variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://t009s:tusharma09@ds145790.mlab.com:45790/todos-collection' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
+
+//'mongodb://t009s:tusharma09@ds145790.mlab.com:45790/todos-collection' ||
